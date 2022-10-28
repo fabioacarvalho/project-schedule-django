a2bfc765a5ac39922aaabb9f936fade492a900ac
Fábio Carvalho <84592352+fabioacarvalho@users.noreply.github.com>
2022-10-27 22:58:59 -0300

    up

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from .models import Contato
 
 def index(request):
-    return render(request, 'contacts/index.html')
+
+    contatos = Contato.objects.all()
+
+    return render(request, 'contacts/index.html', {
+        'contatos': contatos #vai conter todos os dados
+    })
+
+def ver_contato(request, contato_id):
+
+    contato = Contato.objects.get(id=contato_id)
+
+    return render(request, 'contacts/ver_contato.html', {
+        'contato': contato #vai conter todos os dados do contato
+    })
